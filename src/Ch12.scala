1686fcaba237a0f311712e36428cb2e3d405e057
Jimin Hsieh <jimin@ghtinc.com>
2016-02-20 11:03:17 +0800

    Add part of Ch12

--- src/Ch12.scala
+++ src/Ch12.scala
@@ -16,8 +16,32 @@ object Ch12 {
     for (i <- low to high) yield (i, fun(i))
   }
 
+  /**
+    * Exercise 02
+    *
+    * @param array
+    * @return
+    */
+  def largestElement(array: Array[Int]) = {
+    array.reduceLeft(_ max _)
+  }
+
+  /**
+    * Exercise 3
+    *
+    * @param n
+    * @return
+    */
+  def factorialNoLoop(n: Int) = (1 to n).reduceLeft(_ * _)
+
   def main(args: Array[String]) {
     println("### Exercises 1 ###")
     println(values(x => x * x, -5, 5))
+
+    println("\n### Exercises 2 ###")
+    println(largestElement(Array(10, 200, 1, -3, 5, 9)))
+
+    println("\n### Exercises 3 ###")
+    println(factorialNoLoop(5))
   }
 }
